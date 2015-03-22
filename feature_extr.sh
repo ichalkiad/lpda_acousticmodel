3cf0bcd3d4f67f653f73925ad4537911686e270f
YannisH <haljohn@hotmail.com>
2015-03-22 20:12:47 +0200

    Use python script and output binary files with super-vectors

--- feature_extr.sh
+++ feature_extr.sh
@@ -7,17 +7,23 @@ OUTPUT_FDR=/home/yannis/Desktop/LPDA
 
 #Convert .ark to text files
 
-$KALDI/src/featbin/copy-feats ark:$INPUT_FDR/raw_mfcc_train_si284.1.ark ark,t:$OUTPUT_FDR/raw_mfcc_train_si284_1_txt.ark
+#$KALDI/src/featbin/copy-feats ark:$INPUT_FDR/raw_mfcc_train_si284.1.ark ark,t:$OUTPUT_FDR/raw_mfcc_train_si284_1_txt.ark
 
-$KALDI/src/featbin/copy-feats ark:$INPUT_FDR/raw_mfcc_train_si284.2.ark ark,t:$OUTPUT_FDR/raw_mfcc_train_si284_2_txt.ark
+#$KALDI/src/featbin/copy-feats ark:$INPUT_FDR/raw_mfcc_train_si284.2.ark ark,t:$OUTPUT_FDR/raw_mfcc_train_si284_2_txt.ark
 
-#Remove sample delimiters for Julia processing
+echo 'Done converting .ark to .txt.'
 
-./kaldi2txt raw_mfcc_train_si284_1_txt.ark raw_mfcc_train_si284_1_txt_formatted.ark
-./kaldi2txt raw_mfcc_train_si284_2_txt.ark raw_mfcc_train_si284_2_txt_formatted.ark
+#Remove sample delimiters for Julia processing; first set python path in kaldi2txt.py
 
-#Make HDF5 file for Julia/MatLab processing
+#./kaldi2txt.py -i raw_mfcc_train_si284_1_txt.ark -o raw_mfcc_train_si284_1_txt_formatted.ark
+#./kaldi2txt.py -i raw_mfcc_train_si284_2_txt.ark -o raw_mfcc_train_si284_2_txt_formatted.ark
 
-$JULIA txt2julia.jl OUTPUT_FDR/raw_mfcc_train_si284_1_txt_formatted.ark 
+echo 'Removed unnecessary delimiters.'
 
-$JULIA txt2julia.jl OUTPUT_FDR/raw_mfcc_train_si284_2_txt_formatted.ark
+#Make binary file for Julia/MatLab processing
+
+$JULIA txt2julia.jl raw_mfcc_train_si284_1_txt_formatted.ark $OUTPUT_FDR $OUTPUT_FDR 
+
+$JULIA txt2julia.jl raw_mfcc_train_si284_2_txt_formatted.ark $OUTPUT_FDR $OUTPUT_FDR
+
+echo 'Feature file ready for processing.'
