3a7225c285a5881959fdd6121e096c55b7033927
YannisH <haljohn@hotmail.com>
2015-03-22 20:14:41 +0200

    New script structure, breakdown MFCCs file into frames, create and save super-vectors

--- txt2julia.jl
+++ txt2julia.jl
@@ -1,14 +1,28 @@
+#julia <filename> <input directory> <output directory>
+
 using HDF5,JLD
 
-mfcc_file = ARGS[1];
+#ARGS[1] = "/home/yannis/Desktop/LPDA/raw_mfcc_train_si284_1_txt_formatted.ark"
 
-function txt2hdf5(mfcc_file)
+#default delimiter is whitespace
+feature_mat = readdlm(ARGS[2]*"/"*ARGS[1])'
+#save(ARGS[1]*".jld","features",feature_mat)
 
-   #default delimiter is whitespace
-   feature_mat = readdlm("$mfcc_file")';
-   save("$mfcc_file.jld","features",feature_mat);
-   return feature_mat;
+m,n = size(feature_mat)
 
+#construct super-vectors  of features = 9 frames of MFCCs
+s = open(ARGS[3]*"/"*ARGS[1]*".bin", "w+")
+super_feat = mmap_array(Float64, (9*m,n), s)
+
+#for first/last 5 super_vec? Same vectors but different labels??
+#super_feat[1,:] = [mfccs[1,:] mfccs[2,:] mfccs[3,:] mfccs[4,:] mfccs[5,:] mfccs[6,:] mfccs[7,:] mfccs[8,:] mfccs[9,:]]
+
+for i = 5:m-4
+    super_feat[:,i] = [feature_mat[:,i-4]' feature_mat[:,i-3]' feature_mat[:,i-2]' feature_mat[:,i-1]' feature_mat[:,i]' feature_mat[:,i+1]' feature_mat[:,i+2]' feature_mat[:,i+3]' feature_mat[:,i+4]']
 end
 
-mfccs = txt2hdf5(mfcc_file);
+msync(super_feat)
+close(s)
+
+#Needed????
+#save("super_vectors.jld","super_feats",super_feat);
