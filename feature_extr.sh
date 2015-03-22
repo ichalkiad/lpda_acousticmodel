e0586d36742593418271c9b0a75030441e304266
YannisH <haljohn@hotmail.com>
2015-03-22 20:18:09 +0200

    Fix comments

--- feature_extr.sh
+++ feature_extr.sh
@@ -7,16 +7,16 @@ OUTPUT_FDR=/home/yannis/Desktop/LPDA
 
 #Convert .ark to text files
 
-#$KALDI/src/featbin/copy-feats ark:$INPUT_FDR/raw_mfcc_train_si284.1.ark ark,t:$OUTPUT_FDR/raw_mfcc_train_si284_1_txt.ark
+$KALDI/src/featbin/copy-feats ark:$INPUT_FDR/raw_mfcc_train_si284.1.ark ark,t:$OUTPUT_FDR/raw_mfcc_train_si284_1_txt.ark
 
-#$KALDI/src/featbin/copy-feats ark:$INPUT_FDR/raw_mfcc_train_si284.2.ark ark,t:$OUTPUT_FDR/raw_mfcc_train_si284_2_txt.ark
+$KALDI/src/featbin/copy-feats ark:$INPUT_FDR/raw_mfcc_train_si284.2.ark ark,t:$OUTPUT_FDR/raw_mfcc_train_si284_2_txt.ark
 
 echo 'Done converting .ark to .txt.'
 
 #Remove sample delimiters for Julia processing; first set python path in kaldi2txt.py
 
-#./kaldi2txt.py -i raw_mfcc_train_si284_1_txt.ark -o raw_mfcc_train_si284_1_txt_formatted.ark
-#./kaldi2txt.py -i raw_mfcc_train_si284_2_txt.ark -o raw_mfcc_train_si284_2_txt_formatted.ark
+./kaldi2txt.py -i raw_mfcc_train_si284_1_txt.ark -o raw_mfcc_train_si284_1_txt_formatted.ark
+./kaldi2txt.py -i raw_mfcc_train_si284_2_txt.ark -o raw_mfcc_train_si284_2_txt_formatted.ark
 
 echo 'Removed unnecessary delimiters.'
 
