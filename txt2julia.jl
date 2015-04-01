1f82e3822d71fed215795cc31987286419d4b7e6
YannisH <haljohn@hotmail.com>
2015-04-01 12:07:18 +0300

    Update script, use mmap, fix super_feat

--- txt2julia.jl
+++ txt2julia.jl
@@ -1,12 +1,10 @@
-#julia <filename> <input directory> <output directory>
+#julia txt2julia.jl <features_filename> <input directory> <output directory>
 
 using HDF5,JLD
 
-#ARGS[1] = "/home/yannis/Desktop/LPDA/raw_mfcc_train_si284_1_txt_formatted.ark"
-
 #default delimiter is whitespace
 feature_mat = readdlm(ARGS[2]*"/"*ARGS[1])'
-#save(ARGS[1]*".jld","features",feature_mat)
+#save(ARGS[4]*"/"*ARGS[1]*".jld","features",feature_mat)
 
 m,n = size(feature_mat)
 
@@ -14,15 +12,22 @@ m,n = size(feature_mat)
 s = open(ARGS[3]*"/"*ARGS[1]*".bin", "w+")
 super_feat = mmap_array(Float64, (9*m,n), s)
 
-#for first/last 5 super_vec? Same vectors but different labels??
-#super_feat[1,:] = [mfccs[1,:] mfccs[2,:] mfccs[3,:] mfccs[4,:] mfccs[5,:] mfccs[6,:] mfccs[7,:] mfccs[8,:] mfccs[9,:]]
+#save dimensions of array for later opening
+save(ARGS[3]*"/"ARGS[1]*"dims.jld","sv_dims",(9*m,n))
 
-for i = 5:m-4
-    super_feat[:,i] = [feature_mat[:,i-4]' feature_mat[:,i-3]' feature_mat[:,i-2]' feature_mat[:,i-1]' feature_mat[:,i]' feature_mat[:,i+1]' feature_mat[:,i+2]' feature_mat[:,i+3]' feature_mat[:,i+4]']
+for i = 1:m
+    if i<5
+      super_feat[:,i] = [feature_mat[:,i]' feature_mat[:,i]' feature_mat[:,i]' feature_mat[:,i]' feature_mat[:,i]' feature_mat[:,i+1]' feature_mat[:,i+2]' feature_mat[:,i+3]' feature_mat[:,i+4]']'
+      write(s,super_feat[:,i])
+    elseif i>m-4
+      super_feat[:,i] = [feature_mat[:,i-4]' feature_mat[:,i-3]' feature_mat[:,i-2]' feature_mat[:,i-1]' feature_mat[:,i]' feature_mat[:,i]' feature_mat[:,i]' feature_mat[:,i]' feature_mat[:,i]']'
+      write(s,super_feat[:,i])
+    else
+      super_feat[:,i] = [feature_mat[:,i-4]' feature_mat[:,i-3]' feature_mat[:,i-2]' feature_mat[:,i-1]' feature_mat[:,i]' feature_mat[:,i+1]' feature_mat[:,i+2]' feature_mat[:,i+3]' feature_mat[:,i+4]']'
+      write(s,super_feat[:,i])
+    end
 end
 
+write(s,super_feat)
 msync(super_feat)
 close(s)
-
-#Needed????
-#save("super_vectors.jld","super_feats",super_feat);
