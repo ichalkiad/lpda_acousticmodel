13d92ba31ce1ab4e0cb4b59a077bae84192ec321
YannisH <haljohn@hotmail.com>
2015-06-25 09:46:11 +0300

    Minor fixes

--- getPhonesPriors.m
+++ getPhonesPriors.m
@@ -1,4 +1,4 @@
-load('/home/yannis/Desktop/LPDA/phones_.mat');
-phones_priors = tabulate(phones);
-save('phones_priors.mat','phones_priors');
+m=memmapfile('/home/yannis/Desktop/KALDI_norm_var/frameLabels2a.txt.bin','format','double');
+phones_priors = tabulate(m.Data);
+save '/home/yannis/Desktop/KALDI_norm_var/phones_priors.mat' phones_priors -v7.3;
 quit
