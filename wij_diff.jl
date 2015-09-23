e0e8a1c3022b68012ffe3fb4deefdc78bf390fdd
YannisH <haljohn@hotmail.com>
2015-09-23 12:32:28 +0300

    Get neighbours + Wint-Wpen matrix for DNN training

--- wij_diff.jl
+++ wij_diff.jl
@@ -1,16 +1,18 @@
 using HDF5,JLD
 
-Wint = load("/home/ichalkia/WintLPDA14.jld")["Wint"]
-Wpen = load("/home/ichalkia/WpenLPDA14.jld")["Wpen"]
+Wint = load("/media/data/ichalkia/dataset_mono2/WintLPDA14_mono2.jld")["Wint"]
+Wpen = load("/media/data/ichalkia/dataset_mono2/WpenLPDA14_mono2.jld")["Wpen"]
+
+display(size(Wint,2))
 
 Wdiff = Wint-Wpen
 
-Wij_diff = zeros(28,900000)
-neighbours = zeros(28,900000)
-for i=1:900000
+Wij_diff = zeros(28,size(Wint,2))
+neighbours = zeros(28,size(Wint,2))
+for i=1:size(Wint,2)
     neighbours[:,i] = find(Wdiff[:,i].!=0)
     Wij_diff[:,i] = full(Wdiff[find(Wdiff[:,i].!=0),i])  
     display(i)
 end
-h5write("/home/ichalkia/neighb.h5","neighbours",neighbours)
-h5write("/home/ichalkia/Wij_diff.h5","Wij_diff",Wij_diff)
+h5write("/media/data/ichalkia/dataset_mono2/neighbFULL_mono2.h5","neighbours",neighbours)
+h5write("/media/data/ichalkia/dataset_mono2/Wij_diff_mono2.h5","Wij_diff",Wij_diff)
