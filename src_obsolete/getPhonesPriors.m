7bba8c5dd8edc7ee7299abc6e72adcf991a85580
YannisH <haljohn@hotmail.com>
2016-05-02 12:30:50 +0300

    Repo cleanup

--- src_obsolete/getPhonesPriors.m
+++
@@ -1,4 +0,0 @@
-m=memmapfile('/home/yannis/Desktop/KALDI_norm_var/frameLabels2a.txt.bin','format','double');
-phones_priors = tabulate(m.Data);
-save '/home/yannis/Desktop/KALDI_norm_var/phones_priors.mat' phones_priors -v7.3;
-quit
--- src_rest/update_lex.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-sed -i 's/OY0/OY1/g' $1
-sed -i 's/UH0/UH1/g' $1
